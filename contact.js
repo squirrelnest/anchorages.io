d2dfe92d176ca5568128d3f3ac3b989d2f731d09
mikka <mikkanthrope@gmail.com>
2018-02-09 19:39:37 -0800

    Add formspree and jqBoostrapValidation

---
+++ contact.js
@@ -0,0 +1,55 @@
+$(function() {
+
+    $("input,textarea").jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function($form, event, errors) {
+            // additional error messages or events
+        },
+        submitSuccess: function($form, event) {
+            event.preventDefault(); // prevent default submit behaviour
+            // get values from FORM
+            var email = $("input#email").val();
+            var message = $("textarea#message").val();
+
+            // Post to Formspree
+            $.ajax({
+                url: "//formspree.io/info@anchorages.io",
+                type: "POST",
+                dataType: "json",
+                data: {
+                    email: email,
+                    message: message
+                },
+                cache: false,
+                success: function() {
+                    // Success message
+                    $('#success').html("<div class='alert alert-success'>");
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-success')
+                        .append("<strong>Thanks for getting in touch! We will let you know when the beta is ready.</strong>");
+                    $('#success > .alert-success')
+                        .append('</div>');
+
+                    //clear all fields
+                    $('.contactForm').trigger("reset");
+                    ga('send', 'event', 'contact', 'submit', 'success');
+                }
+            })
+        },
+        filter: function() {
+            return $(this).is(":visible");
+        },
+    });
+
+    $("a[data-toggle=\"tab\"]").click(function(e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
+
+
+/*When clicking on Full hide fail/success boxes */
+$('#name').focus(function() {
+    $('#success').html('');
+});
