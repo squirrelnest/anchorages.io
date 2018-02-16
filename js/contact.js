af5d765f3a66efb4aa8f9e58eccdda357cd1def9
mikka <mikkanthrope@gmail.com>
2018-02-15 22:30:33 -0500

    Add way to close success/fail message

--- js/contact.js
+++ js/contact.js
@@ -24,7 +24,7 @@ $(function() {
                 success: function() {
                     // Success message
                     $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                    $('#success > .alert-success').html("<button type='button' class='close' onclick='closeMessage()' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
                         .append("<strong>Thanks for getting in touch! We will let you know when the beta is ready.</strong>");
@@ -52,3 +52,7 @@ $(function() {
 $('#name').focus(function() {
     $('#success').html('');
 });
+
+function closeMessage() {
+  $('#success').html('');
+}
